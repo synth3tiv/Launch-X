3812bd194368593d27e3842c035b71e67fd22a07
Samantha <ada_hack479@hackademy.mx>
2022-11-07 17:06:36 -0600

    Add(JS): while, do while, for, switch

--- 01_MisionFrontEnd/04_JS/01_intro.js
+++ 01_MisionFrontEnd/04_JS/01_intro.js
@@ -87,3 +87,47 @@ else if( number1 === number2){
 else {
   console.log("El número 2 es mayor que número 1");
 }
+
+/* Ciclo condicional */
+console.log("\n---- While ----\n");
+let numberWhile = 5;
+while (numberWhile <= 12) {
+  console.log(numberWhile);
+  numberWhile = numberWhile + 2;
+}
+console.log("Aquí ya salió del while " + numberWhile);
+
+/* Ciclo condicional de una iteración mínimo */
+console.log("\n---- Do While ----\n");
+let numeroDoWhile = 12;
+do {
+    numeroDoWhile = numeroDoWhile + 2;
+    console.log(numeroDoWhile);
+} while (numeroDoWhile < 20);
+console.log("Aquí sale del Do While " + numeroDoWhile);
+
+/* Ciclo for con iteración controlada */
+console.log("\n---- For ----\n");
+let numeroFor = 0
+for (numeroFor ; numeroFor <= 12; numeroFor = numeroFor + 1) {
+  console.log(numeroFor);
+}
+console.log("Aquí salimos del for " + numeroFor);
+
+/* Opciones para evitar anidar condicionales */
+console.log("\n---- Switch ----\n");
+switch (prompt("¿Cómo está el clima?")) {
+  case "lluvioso":
+    console.log("No te vayas a mojar");
+    break;
+  case "soleado":
+    console.log("Ponte bloqueador");
+    break;
+  case "nublado":
+    console.log("Tapate bien");
+    break;
+  default:
+    console.log("No se como está el clima");
+    break;
+}
+console.log("Aquí salimos del Switch");
